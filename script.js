d54aec98e327b58aa444f3d4c798c120b26a0d19
jmclau29 <90084211+jmclau29@users.noreply.github.com>
2025-10-26 20:47:10 +0900

    completes postal code validation.

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let email = document.getElementById('email');
 let emailError = document.getElementById('emailError');
 
 email.addEventListener('input', () => {
-    if (email.validity.valid) {
-        email.className = "";
-        emailError.textContent = "";
-        emailError.className = "error";
-    } else {
-        showError();
-    }
+  if (email.validity.valid) {
+    email.className = "";
+    emailError.textContent = "";
+    emailError.className = "error";
+  } else {
+    showError();
+  }
 })
 
 const countrySelect = document.getElementById("countries");
@@ -16,59 +16,59 @@ const postalCodeField = document.getElementById("postalcode");
 const postalcodeError = document.getElementById("postalcodeError");
 
 function checkPostalCode() {
-    const constraints = {
-        usa: [
-            "^\\b\\d{5}\\b(?:[- ]{1}\\d{4})?$",
-            "US Postal codes must have 5 numbers.",
-        ],
-        canada: [
-            "^(?=[^DdFfIiOoQqUu\\d\\s])[A-Za-z]\\d(?=[^DdFfIiOoQqUu\\d\\s])[A-Za-z]\\s{0,1}\\d(?=[^DdFfIiOoQqUu\\d\\s])[A-Za-z]\\d$",
-            "Canadian postal codes are crazy.",
-        ],
-        uk: [
-            "^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$",
-            "UK Postcodes must be like this.",
-        ],
-    }
+  const constraints = {
+    usa: [
+      "^\\b\\d{5}\\b(?:[- ]{1}\\d{4})?$",
+      "US Postal codes must have 5 numbers.",
+    ],
+    canada: [
+      "^(?=[^DdFfIiOoQqUu\\d\\s])[A-Za-z]\\d(?=[^DdFfIiOoQqUu\\d\\s])[A-Za-z]\\s{0,1}\\d(?=[^DdFfIiOoQqUu\\d\\s])[A-Za-z]\\d$",
+      "Canadian postal codes are crazy.",
+    ],
+    uk: [
+      "^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$",
+      "UK Postcodes must be like this.",
+    ],
+  }
 
-    const country = countrySelect.value;
+  const country = countrySelect.value;
 
 
-    const constraint = new RegExp(constraints[country][0], "");
-    console.log(constraint);
-
-    if (constraint.test(postalCodeField.value)) {
-        postalCodeField.className = "";
-        postalcodeError.textContent = "";
-        postalcodeError.className = "error";
-    } else {
-        showError();
-    }
+  const constraint = new RegExp(constraints[country][0], "");
+  console.log(constraint);
+  console.log(`Validity is ` + constraint.test(postalCodeField.value));
+  if (constraint.test(postalCodeField.value)) {
+    postalCodeField.className = "";
+    postalcodeError.textContent = "";
+    postalcodeError.className = "error";
+  } else {
+    showError();
+  }
 }
 
 countrySelect.addEventListener("change", checkPostalCode);
 postalCodeField.addEventListener("input", checkPostalCode);
 
 function showError() {
-    if (email.validity.typeMismatch) {
-        emailError.textContent = "Please enter an email address!";
-    }
-    email.className = "error";
-    emailError.className = "error active";
 
-    if (postalCodeField.validity.typeMismatch) {
-        switch (countrySelect.value) {
-            case us:
-                postalcodeError.textContent = "US Postal codes must have 5 numbers.";
-                break;
-            case uk:
-                postalcodeError.textContent = "UK Postcodes must be like this.";
-                break;
-            case canada:
-                postalcodeError.textContent = "Canadian postal codes are crazy.";
-                break;
-        }
-    }
+  if (email.validity.typeMismatch) {
+    emailError.textContent = "Please enter an email address!";
+  }
+  email.className = "error";
+  emailError.className = "error active";
+
+  console.log(countrySelect.value);
+  if (countrySelect.value === "usa") {
+    postalcodeError.textContent = "Enter a valid US postal code please."
     postalCodeField.className = "error";
     postalcodeError.className = "error active";
+  } else if (countrySelect.value === "uk") {
+    postalcodeError.textContent = "Enter a valid UK postcode, please.";
+    postalCodeField.className = "error";
+    postalcodeError.className = "error active";
+  } else if (countrySelect.value === "canada") {
+    postalcodeError.textContent = "Enter a valid Canadian postal code, eh?";
+    postalCodeField.className = "error";
+    postalcodeError.className = "error active";
+  }
 }
